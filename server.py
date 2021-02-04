f3e784920f62ade0f5a1d1dcf15e9b2cc15571fd
mggzmn <mggzmn91@gmail.com>
2021-02-04 13:25:26 -0600

    write data into database.txt

--- server.py
+++ server.py
@@ -13,11 +13,20 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         # data = request.form['message']
         data = request.form.to_dict()
+        write_to_file(data)
         return redirect('thankyou.html')
     else:
         return 'something went wrong'
